e0a34e8da5f22d6d74df4dd6bc1f7e85c70d5e9b
averangeall <averangeall@gmail.com>
2013-01-24 11:02:36 +0800

    Add gag title

--- captain_obvious/views.py
+++ captain_obvious/views.py
@@ -11,6 +11,7 @@ br = Browser()
 
 def index(request, gag_id='4863604'):
     br.open_gag(int(gag_id))
+    gag_img_title, _, _, _ = br.get_info_pad()
     gag_img_url = br.get_image_url()
     streams = br.get_comments()
     gag_comments = []
@@ -21,6 +22,7 @@ def index(request, gag_id='4863604'):
                                  'content': reply['content'],
             })
     resp_dict = {
+        'gag_img_title': gag_img_title,
         'gag_img_url': gag_img_url,
         'gag_comments': gag_comments,
     }
